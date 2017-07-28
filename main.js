9da22c5f7a4f41e3ebf968e4d5b69a0059338c49
Nhan Nguyen <nhan_nguyen@brangista.com>
2017-07-28 11:36:17 +0900

    fix missing package in package.json

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types'
-import {Text, View, StyleSheet, Animated} from 'react-native';
+import {Text, View, StyleSheet, Animated, ScrollView} from 'react-native';
 
 import Expo, { Svg } from 'expo';
 import SvgAnimatedLinearGradient from 'react-native-svg-animated-linear-gradient'
@@ -10,7 +10,12 @@ let Facebook = () => {
         <SvgAnimatedLinearGradient
             primaryColor="#e8f7ff"
             secondaryColor="#4dadf7"
-            height={140}>
+            height={140}
+            x1="0"
+            y1="0"
+            x2="100%"
+            y2="100%"
+        >
             <Svg.Rect x="0" y="0" rx="5" ry="5" width="70" height="70"/>
             <Svg.Rect x="80" y="17" rx="4" ry="4" width="300" height="13"/>
             <Svg.Rect x="80" y="40" rx="3" ry="3" width="250" height="10"/>
@@ -23,7 +28,11 @@ let Facebook = () => {
 }
 let Instagram = () => {
     return (
-        <SvgAnimatedLinearGradient height={300}>
+        <SvgAnimatedLinearGradient height={300}
+            x1="100%"
+            y1="0"
+            x2="0"
+            y2="0">
             <Svg.Circle cx="30" cy="30" r="30"/>
             <Svg.Rect x="75" y="13" rx="4" ry="4" width="100" height="13"/>
             <Svg.Rect x="75" y="37" rx="4" ry="4" width="50" height="8"/>
@@ -36,7 +45,12 @@ let Code = () => {
         <SvgAnimatedLinearGradient
             primaryColor="#fff0f6"
             secondaryColor="#f783ac"
-            height={80}>
+            height={80}
+            x1="50%"
+            y1="0"
+            x2="50%"
+            y2="100%"
+        >
             <Svg.Rect x="0" y="0" rx="3" ry="3" width="70" height="10"/>
             <Svg.Rect x="80" y="0" rx="3" ry="3" width="100" height="10"/>
             <Svg.Rect x="190" y="0" rx="3" ry="3" width="10" height="10"/>
@@ -52,10 +66,25 @@ let Code = () => {
 let App = () => {
     return (
         <View style={styles.container}>
+            <ScrollView>
             <Facebook/>
             <Code/>
-            <Instagram/>
-        </View>
+            <Instagram />
+             <SvgAnimatedLinearGradient
+            primaryColor="#e8f7ff"
+            secondaryColor="#4dadf7"
+            height={140}
+        >
+            <Svg.Rect x="0" y="0" rx="5" ry="5" width="70" height="70"/>
+            <Svg.Rect x="80" y="17" rx="4" ry="4" width="300" height="13"/>
+            <Svg.Rect x="80" y="40" rx="3" ry="3" width="250" height="10"/>
+            <Svg.Rect x="0" y="80" rx="3" ry="3" width="350" height="10"/>
+            <Svg.Rect x="0" y="100" rx="3" ry="3" width="200" height="10"/>
+            <Svg.Rect x="0" y="120" rx="3" ry="3" width="360" height="10"/>
+
+        </SvgAnimatedLinearGradient>
+            </ScrollView>
+        </View>    
     )
 }
 
